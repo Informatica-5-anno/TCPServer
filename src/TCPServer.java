c0f38ec332b0a75709ea7d86a8d67ab73a7c79e1
Roberto Spotti <roberto.spotti@gmail.com>
2019-11-05 10:18:13 +0100

    Aggiunta gestione demo HTML

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -19,21 +19,41 @@ import java.io.*;
          String client = address.getHostName();
          int porta = socket.getPort();
          System.out.println("In chiamata Client: "+ client + " porta: " + porta);
-         //Stream di byte utilizzato per la comunicazione via socket
-         //DataInputStream is = new DataInputStream(socket.getInputStream());
-         Scanner is= new Scanner(socket.getInputStream());
+         BufferedReader ibr= new BufferedReader(new InputStreamReader(socket.getInputStream()));
          DataOutputStream os = new DataOutputStream(socket.getOutputStream());
-         while (is.hasNext()) {
-           // String userInput = is.readLine();
-           String userInput = is.nextLine();
-           if (userInput == null || userInput.equals("QUIT"))
-             break;
-           os.writeBytes(userInput + '\n');
+         String ur,userInput="";
+         do {
+        	 ur=ibr.readLine();
+        	 userInput+="\n"+ur;
+        	
+         } while (!ur.isEmpty());
            System.out.println("Il Client ha scritto: " + userInput);
-         }
+           String data=
+        		"<HTML>\r\n" + 
+           		"          <HEAD>\r\n" + 
+           		"             <TITLE>Esempio HTML</TITLE>\r\n" + 
+           		"          </HEAD>\r\n" +  
+           		"          <BODY> \r\n" + 
+           		"            PAGINA HTML DEMO\r\n" + 
+           		"          </BODY>\r\n" + 
+           		"</HTML>\r\n";
+           
+           os.writeBytes("HTTP/1.1 200 OK\r\n" + 
+           		"Date: Tue, 05 Nov 2019 08:37:43 GMT\r\n" + 
+           		"Server: Apache/2.4.6 (Scientific Linux) OpenSSL/1.0.2k-fips mod_fcgid/2.3.9 PHP/5.4.16\r\n" + 
+           		"Last-Modified: Thu, 23 Oct 1997 20:34:37 GMT\r\n" + 
+           		"ETag: \"378-31e3539813140\"\r\n" + 
+           		"Accept-Ranges: bytes\r\n" + 
+           		"Content-Length: "+data.length()+"\r\n" + 
+           		"Keep-Alive: timeout=5, max=100\r\n" + 
+           		"Connection: Keep-Alive\r\n" + 
+           		"Content-Type: text/html\r\n" + 
+           		"\r\n" + data);
+         
+       
          //chiusura della comunicazione con il Client
          os.close();
-         is.close();
+         ibr.close();
          System.out.println("Chiusura chiamata Client: " + client + " su porta:" + porta);
          socket.close();
        }
